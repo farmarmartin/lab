835796a9203ec7a9e2a1a92fd7f83fbcd41bc04d
Martin Job <90215739+farmarmartin@users.noreply.github.com>
2022-09-12 09:46:09 +0200

    Add files via upload

---
+++ numbers/script.php
@@ -0,0 +1,18 @@
+<?php
+$numbers = [];
+
+interface INumbers {
+  public function add($number): void;
+  public function remove($number): void;
+  public function clear() : void;
+  public function sort() : void;
+}
+
+class intelligentNumbers{
+    public function add($number){
+        
+    }
+}
+
+
+?>
