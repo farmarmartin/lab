72e71f595f559fd4f703d0d96ea7527d5be717b1
Martin Job <90215739+farmarmartin@users.noreply.github.com>
2022-09-23 13:19:49 +0200

    Add files via upload

--- numbers/script.php
+++ numbers/script.php
@@ -1,16 +1,55 @@
 <?php
-$numbers = [];
-
-interface INumbers {
-  public function add($number): void;
-  public function remove($number): void;
-  public function clear() : void;
-  public function sort() : void;
+interface INumbers
+{
+    public function add(int $number): void;
+    public function remove(int $number): void;
+    public function clear(): void;
+    public function count(int $number = null): int;
+    public function exists(int $number): bool;
+    public function toStr(string $sep = " - "): string;
 }
 
-class intelligentNumbers{
-    public function add($number){
-        
+$data = [];
+class NumbersSimple implements INumbers{
+    public function __construct()
+    {
+      $this->clear();
+    }
+
+    public function add(int $number): void
+    {
+        $this->data[] = $number;
+    }
+
+    public function remove(int $number): void
+    {
+
+    }
+
+    public function clear(): void
+    {
+
+    } 
+
+    public function count(int $number = null): int
+    {
+        if(!$number){
+          return count($this->data);
+        }
+    }
+
+    public function exists(int $number): bool
+    {
+        foreach ($this->data as $val){
+          if ($val == $number){
+            return true;
+          }
+        }
+    }
+
+    public function toStr(string $sep = " - "): string
+    {
+
     }
 }
 
