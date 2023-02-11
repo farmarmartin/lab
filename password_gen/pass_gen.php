589b6c98a61e1b6b51ff5fbcdc22b80d65ae0acf
farmarmartin <martin.job2906@gmail.com>
2023-02-11 18:05:48 +0100

    passwordTest fixed

--- password_gen/pass_gen.php
+++ password_gen/pass_gen.php
@@ -7,7 +7,6 @@ class Password
     {
         $this->password = '';
         $this->mixed = '';
-
     }
 
     public function generate($lenght, ...$data){ // první parametr je délka hesla a poté už libovolné stringy znaků.
@@ -31,17 +30,20 @@ $big_letters = implode(range("A", "Z"));
 $numbers = implode(range(0, 9));
 $special_chars = "%$><*!()[]{}";
 
-$pass = (new Password)->generate(13, $small_letters, $big_letters, $special_chars, $numbers);
-$pass2 = (new Password)->generate(13, $small_letters, $special_chars);
-
+$dumbPass = (new Password)->generate(10, $small_letters);
 
 
 class passwordTest
 {
-    public $testedPassword;
+    public $testedPassword, $complexityPoints, $small_letters, $big_letters, $numbers, $special_chars;
     public function __construct()
     {
         $this->testedPassword = '';
+        $this->complexityPoints = 0;
+        $this->small_letters = implode(range("a", "z"));
+        $this->big_letters = implode(range("A", "Z"));
+        $this->numbers = implode(range(0, 9));
+        $this->special_chars = "%$><*!()[]{}";
     }
     public function setPassword(string $password): passwordTest
     {
@@ -49,32 +51,79 @@ class passwordTest
         return $this;
     }
 
-    public static function lenghtTest(string $password): bool
+    public function lenghtTest(): bool
     {
-        if (strlen($password) < 14) {
-            echo "lenght test: failed";
+        if (strlen($this->testedPassword) < 8) { //8 znaků se považuje jako minimum pro "bezpečné" heslo
+            echo "lenght test: failed\n";
             return false;
         } else {
-            echo "lenght test: ok";
+            echo "lenght test: ok\n";
             return true;
         }
     }
 
-    public static function complexityTest(string $password, string...$parameters)
-    {
-
-        for ($i = 0; $i < strlen($password); $i++) {
-            for ($x = 0; $x < strlen($parameters[$x]); $x++){
-
-                if (str_contains($parameters[$i], $password[$i])) {
-                    //echo $i.". Run: $password[$i]\n";
-    
-                    }
-    
-                }
+    public function containNum(){
+        for($i = 0; $i < strlen($this->testedPassword) - 1; $i++){
+            if(str_contains($this->numbers, $this->testedPassword[$i])){
+                $this->complexityPoints++;
+                echo "NUM OK\n";
+                break;
             }
-
         }
+        return $this;
+    }
+
+    public function containUppercase(){
+        for($i = 0; $i < strlen($this->testedPassword) - 1; $i++){
+            if(str_contains($this->big_letters, $this->testedPassword[$i])){
+                $this->complexityPoints++;
+                echo "UPPERCASE OK\n";
+                break;
+            }
+        }
+        return $this;
+    }
+
+    public function containLowercase(){
+        for($i = 0; $i < strlen($this->testedPassword) - 1; $i++){
+            if(str_contains($this->small_letters, $this->testedPassword[$i])){
+                $this->complexityPoints++;
+                echo "LOWERCASE OK\n";
+                break;
+            }
+        }
+        return $this;
+    }
+
+    public function containSpecChars(){
+        for($i = 0; $i < strlen($this->testedPassword) - 1; $i++){
+            if(str_contains($this->special_chars, $this->testedPassword[$i])){
+                $this->complexityPoints++;
+                echo "CHARS OK\n";
+                break;
+            }
+        }
+        return $this;
+    }
+    public function result(){
+        if($this->lenghtTest()){
+            switch($this->complexityPoints){
+                case 1:
+                    echo "Heslo není použitelné.\n";
+                    break;
+                case 2:
+                    echo "Heslo není dostatečně komplexní.\n";
+                    break;
+                case 3:
+                    echo "Heslo už se dá použít.\n";
+                    break;
+                case 4:
+                    echo "Heslo je bezpečné.\n";
+                    break;
+            }
+        }
+        return $this;
     }
 }
-passwordTest::complexityTest($pass2->password, $small_letters, $big_letters, $numbers, $special_chars);
+
+$test = (new passwordTest)->setPassword($dumbPass->password)->containLowercase()->containUppercase()->containNum()->containSpecChars()->result();
